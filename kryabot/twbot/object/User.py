6b8cf01c80c3ae7fedc4f27e03b6b46312c9c327
Oskaras Kurmis <kuroskas@gmail.com>
2020-09-20 22:01:48 +0300

    fixes

--- kryabot/twbot/object/User.py
+++ kryabot/twbot/object/User.py
@@ -2,10 +2,10 @@ from object.Base import Base
 
 
 class User(Base):
-    def __init__(self, name, twitch_id, display_name=None):
-        self.name = name
-        self.twitch_id = twitch_id
-        self.display_name = display_name if display_name else name
+    def __init__(self, name: str, twitch_id: int, display_name=None):
+        self.name: str = str(name)
+        self.twitch_id: int = twitch_id
+        self.display_name: str = str(display_name) if display_name else str(name)
         self.db_info = None
 
     def set_db_info(self, db_info):
