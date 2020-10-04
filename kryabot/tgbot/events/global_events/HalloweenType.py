6996afafafe806f0e150a2dec4c6e5ba5adfe78e
Oskaras Kurmis <kuroskas@gmail.com>
2020-10-04 17:31:31 +0300

    spawn ratio fix

--- kryabot/tgbot/events/global_events/HalloweenType.py
+++ kryabot/tgbot/events/global_events/HalloweenType.py
@@ -69,8 +69,8 @@ class HalloweenChannel(Base):
             return False
 
         ratio = HalloweenConfig.calc(channel_size)
-        min_time = max(int(ratio), 5)
-        max_time = max(int(ratio), 15)
+        min_time = min(int(ratio), 60)
+        max_time = max(int(ratio), 180)
         logger.info('Result min: {} max: {} for size {}'.format(min_time, max_time, channel_size))
 
         delay = randint(min_time, max_time)
@@ -85,8 +85,8 @@ class HalloweenChannel(Base):
             return False
 
         ratio = HalloweenConfig.calc(channel_size)
-        min_time = max(int(ratio * 7), 60)
-        max_time = max(int(ratio * 7), 180)
+        min_time = min(int(ratio / 15), 5)
+        max_time = max(int(ratio / 3), 15)
         logger.info('Result min: {} max: {} for size {}'.format(min_time, max_time, channel_size))
 
         delay = randint(min_time, max_time)
