79f76ab399066a5703157a57b00940c9a4146ae7
Oskaras Kurmis <kuroskas@gmail.com>
2021-09-13 21:46:04 +0300

    VK api version catch-up

--- kryabot/api/vk.py
+++ kryabot/api/vk.py
@@ -5,7 +5,7 @@ class VK(Core):
     def __init__(self, cfg=None):
         super().__init__(cfg=cfg)
         self.session_key = self.cfg.getVKConfig()['SESSION']
-        self.vk_version = '5.74'
+        self.vk_version = '5.131'
 
     async def get_song(self, user):
         url = 'https://api.vk.com/method/users.get?user_ids={u}&access_token={token}&v={v}&fields=status'.format(token=self.session_key, u=user, v=self.vk_version)
