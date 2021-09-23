7d2f7438bb70b54a1b1fa450c68d74f5145f9541
Oskaras Kurmis <kuroskas@gmail.com>
2021-09-23 22:12:33 +0300

    Fix data assign logic

--- kryabot/infobot/twitch/TwitchEvents.py
+++ kryabot/infobot/twitch/TwitchEvents.py
@@ -49,16 +49,17 @@ class TwitchEvent(Event):
 
         if self.title and self.title != title:
             self.updated_data.append({'title': title})
-            self.title = title
         if self.language and self.language != language:
             self.updated_data.append({'language': language})
-            self.language = language
         if self.game_id and self.game_id != game_id:
             self.updated_data.append({'game_id': game_id})
-            self.game_id = game_id
         if self.game_name and self.game_name != game_name:
             self.updated_data.append({'game_name': game_name})
-            self.game_name = game_name
+
+        self.title = title
+        self.language = language
+        self.game_id = game_id
+        self.game_name = game_name
 
     def is_start(self)->bool:
         return self.raw['subscription']['type'] == EventSubType.STREAM_ONLINE.key and not self.updated_data
