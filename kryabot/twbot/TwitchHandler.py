9c79d110aa17a74f5a039ccce13b84edcca519f6
Oskaras Kurmis <kuroskas@gmail.com>
2021-09-23 18:03:30 +0300

    Fixed convert to list

--- kryabot/twbot/TwitchHandler.py
+++ kryabot/twbot/TwitchHandler.py
@@ -434,7 +434,7 @@ class TwitchHandler(Base):
                 continue
 
             current_scopes = auth[0]['scope'].split(' ')
-            channel_events = set(filter(lambda row: int(row['condition']['broadcaster_user_id']) == int(channel.tw_id), current_events['data']))
+            channel_events = list(filter(lambda row: int(row['condition']['broadcaster_user_id']) == int(channel.tw_id), current_events['data']))
 
             for topic in required_topics:
                 have_scope = False
