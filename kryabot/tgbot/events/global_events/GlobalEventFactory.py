4aebaf28ed2b4acb11302e6dc021aa49509362fb
Oskaras Kurmis <kuroskas@gmail.com>
2021-09-25 19:02:16 +0300

    Update factory for new event type

--- kryabot/tgbot/events/global_events/GlobalEventFactory.py
+++ kryabot/tgbot/events/global_events/GlobalEventFactory.py
@@ -7,6 +7,7 @@ from tgbot.events.global_events.WinterEventProcessor import WinterEventProcessor
 class GlobalEventFactory:
     factory = {
         "halloween": HalloweenEventProcessor.get_instance(),
+        "halloween_2021": HalloweenEventProcessor.get_instance(),
         "winter": WinterEventProcessor.get_instance(),
         "easter": EasterEventProcessor.get_instance(),
             }
